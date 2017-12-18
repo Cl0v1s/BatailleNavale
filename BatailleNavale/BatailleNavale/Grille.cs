5eec4ce8798bb4f44e365ec271651ee8638f8f92
Clovis Portron <chaipokoi@gmail.com>
2017-12-18 08:42:31 +0100

    Ajout grille

--- BatailleNavale/BatailleNavale/Grille.cs
+++ BatailleNavale/BatailleNavale/Grille.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace BatailleNavale
 {
-    class Program
+    class Grille
     {
-        static void Main(string[] args)
+        public static void AfficherGrille(int[][] grille)
         {
-           Grille.AfficherGrille
+
         }
     }
 }
