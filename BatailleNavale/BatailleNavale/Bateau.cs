492a330c348a33b99f5c5d4fd4bd217658c8d00f
Clovis Portron <chaipokoi@gmail.com>
2017-12-22 19:13:28 +0100

    Ajout de commentaires aux méthodes, ajout de la gestion de la grille dans les fonctions associées aux bateaux, ajout de getters

--- BatailleNavale/BatailleNavale/Bateau.cs
+++ BatailleNavale/BatailleNavale/Bateau.cs
@@ -14,7 +14,14 @@ namespace BatailleNavale
             COLONNE = 1
         }
 
+        /// <summary>
+        /// Nombre de type de bateaux différents 
+        /// </summary>
         public const int NombreTypesBateaux = 5;
+
+        /// <summary>
+        /// Enumération des différents types de bateaux existants
+        /// </summary>
         public enum TYPES
         {
             CONTRETORPILLEUR = 0,
@@ -23,6 +30,10 @@ namespace BatailleNavale
             CUIRASSE = 3,
             PORTEAVION = 4,
         };
+
+        /// <summary>
+        /// Tableau contenant la longueur des différents types de bateaux 
+        /// </summary>
         public static int[] LongueurBateaux =
         {
             2,
@@ -32,13 +43,52 @@ namespace BatailleNavale
             5
         };
 
-
+        // Vie et position des bateaux du joueur 1
         public static int[,] PositionBateauxJ1 = new int[Bateau.NombreTypesBateaux, 4];
         public static int[] VieBateauxJ1 = new int[Bateau.NombreTypesBateaux];
 
+        // Vie et position des bateaux du joueur 2
         public static int[,] PositionBateauxJ2 = new int[Bateau.NombreTypesBateaux, 4];
         public static int[] VieBateauxJ2 = new int[Bateau.NombreTypesBateaux];
 
+
+        public static int[,] ObtenirPositionBateauxJoueur(int joueur)
+        {
+            if (joueur == 1)
+                return Bateau.PositionBateauxJ1;
+            else if (joueur == 2)
+                return Bateau.PositionBateauxJ2;
+            throw new Exception("L'index de joueur ne peut être que 1 ou 2.");
+        }
+
+        public static int[] ObtenirVieBateauxJoueur(int joueur)
+        {
+            if (joueur == 1)
+                return Bateau.VieBateauxJ1;
+            else if (joueur == 2)
+                return Bateau.VieBateauxJ2;
+            throw new Exception("L'index de joueur ne peut être que 1 ou 2.");
+        }
+
+        /// <summary>
+        /// Retourne l'alignement du bateau de type type du joueur joueur 
+        /// </summary>
+        /// <param name="joueur">Joueur dont on doit retourner l'alignement d'un bateau</param>
+        /// <param name="type">Type de bateau dont on doit retourner l'alignement</param>
+        /// <returns>L'alignement du bateau</returns>
+        public static ALIGNEMENT ObtenirAlignementBateau(int joueur, Bateau.TYPES type)
+        {
+            int[,] positionbateaux = Bateau.ObtenirPositionBateauxJoueur(joueur);
+            if (positionbateaux[(int)type, 0] == positionbateaux[(int)type, 2])
+                return ALIGNEMENT.COLONNE;
+            else
+                return ALIGNEMENT.LIGNE;
+        }
+
+        /// <summary>
+        /// Place les bateaux du joueur passé en paramètre de manière aléatoire
+        /// </summary>
+        /// <param name="joueur">Joueur dont on doit placer les bateaux</param>
         public static void PlacerBateauxAuHasard(int joueur)
         {
             Random rnd = new Random();
@@ -49,62 +99,79 @@ namespace BatailleNavale
             int y1 = 0;
             for(int i = 0; i < Bateau.NombreTypesBateaux; i++)
             {
-                alignement = (Bateau.ALIGNEMENT)rnd.Next(0, 2);
-                x = rnd.Next(0, Grille.LargeurGrille);
-                y = rnd.Next(0, Grille.HauteurGrille);
-
-                if (alignement == Bateau.ALIGNEMENT.LIGNE)
+                int essais = 0;
+                do
                 {
-                    if (x + Bateau.LongueurBateaux[i] > Grille.LargeurGrille)
-                        x = x - Bateau.LongueurBateaux[i];
-                    y1 = y;
-                    x1 = x + Bateau.LongueurBateaux[i];
-                }
-                if (alignement == Bateau.ALIGNEMENT.COLONNE && x + Bateau.LongueurBateaux[i] > Grille.HauteurGrille)
-                    y = y - Bateau.LongueurBateaux[i];
+                    essais++;
+                    alignement = (Bateau.ALIGNEMENT)rnd.Next(0, 2);
+                    x = rnd.Next(0, Grille.LargeurGrille);
+                    y = rnd.Next(0, Grille.HauteurGrille);
 
-                for(int j = 0; j < i; j++)
-                {
-
+                    if (alignement == Bateau.ALIGNEMENT.LIGNE)
+                    {
+                        if (x + Bateau.LongueurBateaux[i] > Grille.LargeurGrille)
+                            x = x - Bateau.LongueurBateaux[i];
+                        y1 = y;
+                        x1 = x + Bateau.LongueurBateaux[i];
+                    }
+                    if (alignement == Bateau.ALIGNEMENT.COLONNE && x + Bateau.LongueurBateaux[i] > Grille.HauteurGrille)
+                        y = y - Bateau.LongueurBateaux[i];
                 }
+                while (Bateau.VerifierColisionBateaux(joueur, x, y, x1, y1, (Bateau.TYPES)i) == false && essais < 200);
+                if (essais >= 200)
+                    throw new Exception("Impossible de placer tous les bateaux sur la grille associée au joueur J" + joueur);
+                Bateau.PlacerBateau(joueur, (Bateau.TYPES)i, x, y, alignement);
             }
         }
 
-        public static bool VerifierColisionBateaux(int joueur, Bateau.TYPES type1, Bateau.TYPES type2)
+        /// <summary>
+        /// Détermine si les positions passées en paramètres sont libres ou occupées par un bateau
+        /// </summary>
+        /// <param name="joueur">Joueur dont on doit tester les bateaux</param>
+        /// <param name="x1">Position x du premier point de la droite à tester</param>
+        /// <param name="y1">Position y du premier point de la droite à tester</param>
+        /// <param name="x2">Position x du deuxième point de la droite à tester</param>
+        /// <param name="y2">Position y du deuxième point de la droite à tester</param>
+        /// <param name="type">Type de bateau associé aux points à tester</param>
+        /// <returns>Vrai si position libre, Faux si position occupée</returns>
+        public static bool VerifierColisionBateaux(int joueur, int x1, int y1, int x2, int y2, Bateau.TYPES type)
         {
-            int[,] positionBateaux = null;
-            if (joueur == 1)
+            int[,] grille = Grille.ObtenirGrilleJoueur(joueur);
+            int[,] positionBateaux = Bateau.ObtenirPositionBateauxJoueur(joueur);
+            for (int o = 0; o < Bateau.LongueurBateaux[(int)type]; o++)
             {
-                positionBateaux = Bateau.PositionBateauxJ1;
-            }
-            else if (joueur == 2)
-            {
-                positionBateaux = Bateau.PositionBateauxJ2;
-            }
-            
-            //TODO: faire ce truc
+                // Si le bateau est aligné en colonne
+                if (Bateau.ObtenirAlignementBateau(joueur, type) == ALIGNEMENT.COLONNE)
+                {
+                    if (grille[x1, y1 + o] != (int)Grille.Cases.VIDE)
+                        return false;
+                }
+                else //sinon le bateau est aligné en ligne
+                {
+                    if (grille[x1 + o, y1] != (int)Grille.Cases.VIDE)
+                        return false;
+                }
 
-            return false;
+            }
+            return true;
         }
 
-        public static bool PlacerBateau(int joueur, Bateau.TYPES type, int x, int y, Bateau.ALIGNEMENT alignement)
+        /// <summary>
+        /// Paramètre les positions du bateau passé en paramètre dans la liste des positions de bateau
+        /// </summary>
+        /// <param name="joueur">Joueur associé au bateau à placer</param>
+        /// <param name="type">Type de bateau à placer</param>
+        /// <param name="x">Position x du premier point de la droite associée au bateau à placer</param>
+        /// <param name="y">Position y du premier point de la droite associée au bateau à placer</param>
+        /// <param name="alignement">Alignement du bateau à placer</param>
+        public static void PlacerBateau(int joueur, Bateau.TYPES type, int x, int y, Bateau.ALIGNEMENT alignement)
         {
-            int[,] positionBateaux = null;
-            int[] vieBateaux = null;
+            int[,] positionBateaux = Bateau.ObtenirPositionBateauxJoueur(joueur);
+            int[] vieBateaux = Bateau.ObtenirVieBateauxJoueur(joueur);
             int x1;
             int y1;
-            if(joueur == 1)
-            {
-                positionBateaux = Bateau.PositionBateauxJ1;
-                vieBateaux = Bateau.VieBateauxJ1;
-            }
-            else if(joueur == 2)
-            {
-                positionBateaux = Bateau.PositionBateauxJ2;
-                vieBateaux = Bateau.VieBateauxJ2;
-            }
+            
 
-            //TODO: vérifier que le bateau rentre dans la grille return false sinon
             if (alignement == ALIGNEMENT.LIGNE)
             {
                 x1 = x + Bateau.LongueurBateaux[(int)type];
@@ -116,28 +183,44 @@ namespace BatailleNavale
                 x1 = x;
             }
 
+            if (x < 0 || y < 0 || x1 >= Grille.LargeurGrille || y1 >= Grille.LargeurGrille)
+                throw new Exception("Impossible de placer le bateau. Il se trouve en dehors de la grille.");
+
             vieBateaux[(int)type] = Bateau.LongueurBateaux[(int)type];
             positionBateaux[(int)type,0] = x;
             positionBateaux[(int)type,1] = y;
             positionBateaux[(int)type, 2] = x1;
             positionBateaux[(int)type, 3] = y1;
-            return true;
         }
 
-        public static void ToucherBateau(int joueur, Bateau.TYPES type)
+
+
+        /// <summary>
+        /// Modifie la vie du bateau passé en paramètre associé au joueur passé en paramètres
+        /// </summary>
+        /// <param name="joueur">Joueur dont on doit modifier un bateau</param>
+        /// <param name="type">Type de bateau dont on doit modifier la vie </param>
+        /// <param name="x">Position x à la quelle le bateau a été touché</param>
+        /// <param name="y">Position y à laquelle le bateau a été touché</param>
+        public static void ToucherBateau(int joueur, Bateau.TYPES type, int x, int y)
         {
-            int[] vieBateaux = null;
-            if (joueur == 1)
-            {
-                vieBateaux = Bateau.VieBateauxJ1;
-            }
-            else if (joueur == 2)
-            {
-                vieBateaux = Bateau.VieBateauxJ2;
-            }
+            int[,] positionbateau = Bateau.ObtenirPositionBateauxJoueur(joueur);
+            int[] vieBateaux = Bateau.ObtenirVieBateauxJoueur(joueur);
+            int[,] grille = Grille.ObtenirGrilleJoueur(joueur);
+            grille[x, y] = (int)Grille.Cases.TOUCHE;
             vieBateaux[(int)type] = vieBateaux[(int)type] - 1;
             if (vieBateaux[(int)type] <= 0)
+            {
+                // On marque la bateau comme coulé sur la grille
+                for (int o = 0; o < Bateau.LongueurBateaux[(int)type]; o++)
+                {
+                    if (Bateau.ObtenirAlignementBateau(joueur, type) == ALIGNEMENT.COLONNE)
+                        grille[positionbateau[(int)type, 0], positionbateau[(int)type, 1]+o] = (int)Grille.Cases.COULE;
+                    else
+                        grille[positionbateau[(int)type, 0]+o, positionbateau[(int)type, 1]] = (int)Grille.Cases.COULE;
+                }
                 Console.WriteLine("Un bateau a été coulé.");
+            }
         }
 
         public static bool Tirer(int joueur, int x, int y)
@@ -189,7 +272,7 @@ namespace BatailleNavale
             }
 
             if (trouve == true)
-                Bateau.ToucherBateau(Joueur.ObtenirAutreJoueur(joueur), (Bateau.TYPES)(i - 1));
+                Bateau.ToucherBateau(Joueur.ObtenirAutreJoueur(joueur), (Bateau.TYPES)(i - 1), x , y);
             return trouve;
 
         }
--- BatailleNavale/BatailleNavale/Grille.cs
+++ BatailleNavale/BatailleNavale/Grille.cs
@@ -9,10 +9,59 @@ namespace BatailleNavale
     class Grille
     {
 
+        /// <summary>
+        /// Enumération des états possibles pour les cases des grilles
+        /// </summary>
+        public enum Cases
+        {
+            VIDE = 0,
+            PLEIN = 1,
+            TOUCHE = 2,
+            COULE = 3
+        };
+
         public const int LargeurGrille = 10;
         public const int HauteurGrille = 10;
 
+        /// <summary>
+        /// Grille de jeu du joueur 1
+        /// </summary>
+        public static int[,] GrilleJ1 = new int[LargeurGrille, HauteurGrille];
 
+        /// <summary>
+        /// Grille contenant les positions et les coups joués du joueur 1 sur le joueur 2
+        /// </summary>
+        public static int[,] GrilleDecouverteJ1 = new int[LargeurGrille, HauteurGrille];
+
+        /// <summary>
+        /// Grille de jeu du joueur 2
+        /// </summary>
+        public static int[,] GrilleJ2 = new int[LargeurGrille, HauteurGrille];
+
+        /// <summary>
+        /// Grille contenant les positions et les coups joués du joueur 2 sur le joueur 1
+        /// </summary>
+        public static int[,] GrilleDecouverteJ2 = new int[LargeurGrille, HauteurGrille];
+
+        /// <summary>
+        /// Retourne la grille du joueur passé en paramètre
+        /// </summary>
+        /// <param name="joueur">Joueur dont la grille doit être retournée</param>
+        /// <returns>La grille du joueur passé en paramètre</returns>
+        public static int[,] ObtenirGrilleJoueur(int joueur)
+        {
+            if (joueur == 1)
+                return Grille.GrilleJ1;
+            else if (joueur == 2)
+                return Grille.GrilleJ2;
+            throw new Exception("L'index de joueur ne peut être que 1 ou 2.");
+        }
+
+
+        /// <summary>
+        /// Affiche la grille passée en paramètre
+        /// </summary>
+        /// <param name="grille">grille à afficher</param>
         public static void AfficherGrille(int[,] grille)
         {
             Console.WriteLine("     A  B  C  D  E  F  G  H  I  J");
@@ -34,7 +83,7 @@ namespace BatailleNavale
                 }
                 for (int j = 0; j < 10; j++)
                 {
-
+                    //TODO: afficher les caractères correspondants aux états
                     Console.Write(grille[i, j]);
                     if (j != 9)
                     {
@@ -46,36 +95,53 @@ namespace BatailleNavale
             Console.WriteLine("   +--+--+--+--+--+--+--+--+--+--+");
 
         }
-        public static void remplirgrille(int[,]grille,int [,]positionbateau)
+
+        /// <summary>
+        /// Met à jour la grille à partir de la position de plusieurs bateaux
+        /// </summary>
+        /// <param name="grille">Grille à mettre à jour</param>
+        /// <param name="positionbateaux">Tableau en 2 dimensions contenant les positions de plusieurs bateaux</param>
+        public static void mettreaJourGrillePlusieursBateaux(int[,]grille,int [,]positionbateaux)
         {
            
             for (int j = 0; j < (Bateau.NombreTypesBateaux-1); j++)
             {
-                    int x1 = positionbateau[j, 0];
-                    int y1 = positionbateau[j, 1];
-                    int x2 = positionbateau[j, 2];
-                    int y2 = positionbateau[j, 3];
-                    if (x1==x2)
-                    {
-                        for (int i=y1; i<=y2;i++)
-                        {
-                            grille[x1, i] = j;
-                        }
-                    }
-                    if(y1==y2)
-                    {
-                        
-                        for (int i=x1; i<=x2;i++)
-                        {
-                            grille[i, y1] = j;
-                        }
-                    }
-            
+                int x1 = positionbateaux[j,0];
+                int y1 = positionbateaux[j,1];
+                int x2 = positionbateaux[j,2];
+                int y2 = positionbateaux[j,3];
+                Grille.mettreaJourGrilleUnBateau(grille, x1, y1, x2, y2);
             }
-            AfficherGrille(grille); 
-
-
         }
 
+        /// <summary>
+        /// Met à jour la grille à partir de la position d'un bateau
+        /// </summary>
+        /// <param name="grille">Grille à mettre à jour</param>
+        /// <param name="x1">Position x du premier point de la droite</param>
+        /// <param name="y1">Position y du premier point de la droite</param>
+        /// <param name="x2">Position x du deuxième point de la droite</param>
+        /// <param name="y2">Position y du deuxième point de la droite</param>
+        public static void mettreaJourGrilleUnBateau(int[,] grille, int x1, int y1, int x2, int y2)
+        {
+
+            if (x1 == x2)
+            {
+                for (int i = y1; i <= y2; i++)
+                {
+                    grille[x1, i] = j;
+                }
+            }
+            if (y1 == y2)
+            {
+
+                for (int i = x1; i <= x2; i++)
+                {
+                    grille[i, y1] = j;
+                }
+            }
+        }
     }
+
+
 }
